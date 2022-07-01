39523d0f83e09f5949260714ffcfa679e54f907e
jabber <2898117012@qq.com>
2022-07-02 01:01:31 +0800

    init

--- aifanfan.php
+++ aifanfan.php
@@ -1,1 +1,16 @@
 <?php
+/**
+ * Plugin Name: Aifanfan
+ * Plugin URI: https://www.shopeo.cn
+ * Description: Integrate Baidu Aifanfan CRM management system to collect customer leads.
+ * Author: Shopeo
+ * Version: 0.0.1
+ * Author URI: https://www.shopeo.cn
+ * License: GPL2+
+ * Text Domain: aifanfan
+ * Domain Path: /languages
+ * Requires at least: 5.9
+ * Requires PHP: 5.6
+ */
+
+
